5d5730cf45f7448ad6c78ff5d046cf5a91fbac6a
Alison Hawke <44695181+BritishKoalaTea@users.noreply.github.com>
2019-09-14 06:15:33 -0500

    Tweaking tests after initial trial
    To make sure the dumb solution doesn't continue to actually work

--- test/maths.test.js
+++ test/maths.test.js
@@ -3,12 +3,18 @@ const maths = require('../maths.js');
 describe('mathematics addition and multiplication tests', () => {
   test('should add 1 + 2 to equal 3', () => {
     expect(maths.add(1, 2)).toBe(3);
+  });
+  
+   test('should add two numbers correctly', () => {
     expect(maths.add(5, 13)).toBe(18);
     expect(maths.add(10, -2)).toBe(8);
   });
   
-  test('should multiply two numbers correctly', () => {
+  test('should multiply 7 x 9 and get 63', () => {
     expect(maths.multiply(7, 9)).toBe(63);
+  });
+  
+    test('should multiply two numbers correctly', () => {
     expect(maths.multiply(-2, 5)).toBe(-10);
     expect(maths.multiply(6, 3.1415927)).toBe(18.8495562);
   });
