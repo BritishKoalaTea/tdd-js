20235978902ea42276a7f2f4a6b76bd3d709ffbb
Alison Hawke <44695181+BritishKoalaTea@users.noreply.github.com>
2019-09-30 13:52:57 -0500

    Update stringy.test.js
    missing semicolons

--- test/stringy.test.js
+++ test/stringy.test.js
@@ -3,22 +3,22 @@ const stringy = require('../stringy.js');
 describe('String operations', () => {
 
     test('Can make a greeting', () => {
-        result = stringy.joinTwoStrings('hello', ' world')
+        result = stringy.joinTwoStrings('hello', ' world');
         expect(result).toEqual('hello world');        
     });
 
     test('Can make a longer greeting', () => {
-        result = stringy.joinTwoStrings('Greetings and salutations', ' to you, good Person!')
+        result = stringy.joinTwoStrings('Greetings and salutations', ' to you, good Person!');
         expect(result).toEqual('Greetings and salutations to you, good Person!');
     });
 
     test('Can get the thirteenth letter of the alphabet', () => {
-        result = stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 13)
+        result = stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 13);
         expect(result).toEqual('m');
     });
     
     test('Can get the Nth letter of the alphabet', () => {
-        result = stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 5)
+        result = stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 5);
         expect(result).toEqual('e');
         expect(stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 8)).toEqual('h');
         expect(stringy.getTheNthLetter('abcdefghijklmnopqrstuvwxyz', 25)).toEqual('y');
